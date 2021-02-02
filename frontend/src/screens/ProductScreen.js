24a7a0e000206dd5a3fa153440b660b58aadeb6b
Chidambararajab <chidambararaja97@gmail.com>
2021-02-02 23:27:19 +0530

    Commit => Added Nodemon

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import axios from "axios";
 
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((product) => product._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [match]);
   return (
     <>
-      <Link to="/">Go Back</Link>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
       <Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
