80cac0df89309968d11ee43917a97eb0198b4f01
Chidambararajab <chidambararaja97@gmail.com>
2021-02-02 20:30:12 +0530

    Commit => Added HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { Container } from "react-bootstrap";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
   return (
     <>
-      <h1>React</h1>
+      <Header />
+      <main>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
