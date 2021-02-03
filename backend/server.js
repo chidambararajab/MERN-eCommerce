f536a8190bac3904468c4b28b07098cda92897d6
Chidambararajab <chidambararaja97@gmail.com>
2021-02-03 14:48:35 +0530

    Commit => Added .env file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
 const products = require("./data/products");
+
+// Used to set env
+dotenv.config();
 
 const app = express();
 
@@ -17,4 +20,11 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("###Server Running Successfully###"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `### Server Running Successfully in ${process.env.NODE_ENV} mode on port ${PORT} ###`
+  )
+);
